1da8ce713327c3c35ee0e61d9a28b6646f1a5c5e
chaimaamounaji <chaimaa.mounaji@outlook.com>
2026-02-04 11:57:10 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,30 +1,35 @@
---- Create two pluggable databases: one for mixed data and one for clean
-CREATE PLUGGABLE DATABASE PDB_MIXED
-ADMIN USER pdb_mixed_admin IDENTIFIED BY oracle
-FILE_NAME_CONVERT = ('pdbseed', 'pdb_mixed');
+--- Create two pluggable databases: one primary and second standby
+CREATE PLUGGABLE DATABASE PDB_VACCINATION_PRIMARY
+ADMIN USER pdb_primary_admin IDENTIFIED BY oracle
+FILE_NAME_CONVERT = ('pdbseed', 'pdb_vaccination_primary');
 --
-CREATE PLUGGABLE DATABASE PDB_CLEAN_SECURE
-ADMIN USER pdb_clean_admin IDENTIFIED BY oracle
-FILE_NAME_CONVERT = ('pdbseed', 'pdb_clean_secure');
+CREATE PLUGGABLE DATABASE PDB_VACCINATION_STANDBY
+ADMIN USER pdb_standby_admin IDENTIFIED BY oracle
+FILE_NAME_CONVERT = ('pdbseed', 'pdb_vaccination_standby');
 
-ALTER PLUGGABLE DATABASE PDB_MIXED OPEN;
-ALTER PLUGGABLE DATABASE PDB_CLEAN_SECURE OPEN;
+ALTER PLUGGABLE DATABASE PDB_VACCINATION_PRIMARY OPEN;
+ALTER PLUGGABLE DATABASE PDB_VACCINATION_STANDBY OPEN;
 
-ALTER SESSION SET CONTAINER = PDB_MIXED;
+ALTER SESSION SET CONTAINER = PDB_VACCINATION_PRIMARY;
 
---create table to hold raw bank transactions
-CREATE TABLE bank_transactions_raw (
-    transaction_id NUMBER,
-    account_id NUMBER,
-    amount NUMBER,
-    transactions_today NUMBER,
-    country_change CHAR(1),
-    failed_attempts NUMBER,
-    transaction_hour NUMBER
+--create table to hold raw vaccination record
+-- PRIMARY PDB : données de vaccination brutes
+ALTER SESSION SET CONTAINER = PDB_VACCINATION_PRIMARY;
+
+CREATE TABLE vaccination_records_raw (
+    vaccination_id NUMBER,
+    patient_id NUMBER,
+    vaccin_id NUMBER,
+    centre_id NUMBER,
+    dose_number NUMBER,
+    adverse_effect CHAR(1),
+    vaccination_date DATE
 );
 
+
 --create wallet and encryption key for secure pluggable database
-ALTER SESSION SET CONTAINER = PDB_CLEAN_SECURE;
+ALTER SESSION SET CONTAINER = PDB_VACCINATION_STANDBY;
+
 ADMINISTER KEY MANAGEMENT CREATE KEYSTORE '/opt/oracle/wallet'
 IDENTIFIED BY walletpass;
 
@@ -36,24 +41,30 @@ IDENTIFIED BY walletpass;
 ADMINISTER KEY MANAGEMENT SET KEY
 IDENTIFIED BY walletpass WITH BACKUP;
 
-ALTER SESSION SET CONTAINER = PDB_CLEAN_SECURE;
+ALTER SESSION SET CONTAINER = PDB_VACCINATION_STANDBY;
 --create tablespace with encryption
-CREATE TABLESPACE ts_secure_data
-DATAFILE 'ts_secure_data01.dbf' SIZE 100M
+-- Encrypted tablespace (STANDBY)
+CREATE TABLESPACE ts_secure_vaccination
+DATAFILE 'ts_secure_vaccination01.dbf' SIZE 100M
 ENCRYPTION USING 'AES256'
-DEFAULT STORAGE(ENCRYPT);
+DEFAULT STORAGE (ENCRYPT);
+
 
 
 --create table and indexes in the encrypted tablespace
-CREATE TABLE bank_transactions_clean (
-    transaction_id NUMBER,
-    account_id NUMBER,
-    amount NUMBER,
-    transaction_date DATE
+CREATE TABLE vaccination_record_secure (
+    vaccination_id NUMBER,
+    patient_id NUMBER,
+    vaccin_id NUMBER,
+    centre_id NUMBER,
+    dose_number NUMBER,
+    adverse_effect CHAR(1),
+    vaccination_date DATE
 )
 
-TABLESPACE ts_secure_data;
-CREATE INDEX idx_account_id ON bank_transactions_clean(account_id)
-TABLESPACE ts_secure_data;
-CREATE INDEX idx_transaction_date ON bank_transactions_clean(transaction_date)
-TABLESPACE ts_secure_data;
+TABLESPACE ts_secure_vaccination;
+CREATE INDEX idx_patient_id ON vaccination_records_secure(patient_id)
+TABLESPACE ts_secure_vaccination;
+CREATE INDEX idx_vaccination_date ON vaccination_records_secure(vaccination_date)
+
+TABLESPACE ts_secure_vaccination;
